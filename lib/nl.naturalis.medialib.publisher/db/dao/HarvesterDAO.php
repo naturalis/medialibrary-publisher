6f7f55357d74bbddb18765f20e450c4fc72bb9ff
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2017-12-13 10:36:14 +0100

    update query corrected

--- lib/nl.naturalis.medialib.publisher/db/dao/HarvesterDAO.php
+++ lib/nl.naturalis.medialib.publisher/db/dao/HarvesterDAO.php
@@ -127,7 +127,7 @@ class HarvesterDAO extends BaseDAO {
 	public function setBackupOkForMediaFile ($id, $fileInfo)
 	{
 		$sql = 'UPDATE media 
-			SET (source_file_sha256 = ?, source_file_aws_uri = ?, source_file_backup_created = ?, backup_ok = 1) 
+			SET source_file_sha256 = ?, source_file_aws_uri = ?, source_file_backup_created = ?, backup_ok = 1 
 			WHERE id = ?';
 		$stmt = $this->_pdo->prepare($sql);
 		$stmt->bindValue(1, $fileInfo->sha256);
